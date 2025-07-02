3248d66ed8a74db6437e7ebcf8e4de7096914fef
Rahul Kumar <kumarrahul771996@gmail.com>
2025-07-02 20:13:15 +0530

    add: reusable Navbar Footer and ProfileCard

--- FirstProject/src/App.jsx
+++ FirstProject/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { Navbar } from './day02/ReusableNavbar'
+import { Footer } from './day02/ReusableFooter'
+import { Card } from './day02/ReusableProfileCard'
+import { MyApp } from './day02/ReusableProfileCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div class= "flex items-center justify-center h-screen bg-teal-900">
-      <h1 class="text-3xl font-bold text-white">Hello React 👋</h1>
+    // <div class= "flex items-center justify-center h-screen bg-teal-900">
+    //   <h1 class="text-3xl font-bold text-white">Hello React 👋</h1>
+    // </div>
+
+    // day02 
+    <div>
+      {/* <Navbar/> */}
+      {/* <Footer/> */}
+      <MyApp/>
     </div>
   )
 }
---
+++ FirstProject/src/day02/ReusableFooter.jsx
@@ -0,0 +1,26 @@
+export const Footer = () => {
+  return <footer>
+    <ul>
+        <li>Fitness Dashboard</li>
+        <li>Watch Videos</li>
+        <li>Privacy Policy</li>
+    </ul>
+    <ul>
+        <li>Services</li>
+        <li>Discord</li>
+        <li>Terms & Conditions</li>
+    </ul>
+    <ul>
+        <li>
+            <p>© 2025 Fitness Dashboard. All Rights Reserved.</p>
+        </li>
+        <li>Happy Coding</li>
+    </ul>
+    <ul>
+        <li><a href="#">🎮</a></li>
+        <li><a href="#">🐓</a></li>
+        <li><a href="#">💻</a></li>
+        <li><a href="#">🏀</a></li>
+    </ul>
+  </footer>;
+};
