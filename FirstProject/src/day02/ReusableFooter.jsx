009a554b5369a9d0acd8a20d4f9505ea4039fd55
Rahul Kumar <kumarrahul771996@gmail.com>
2025-07-03 03:07:23 +0530

    feat: style with inline css in JSX file

--- FirstProject/src/day02/ReusableFooter.jsx
+++ FirstProject/src/day02/ReusableFooter.jsx
@@ -1,22 +1,44 @@
+const customStyle = {
+    footer: {
+        backgroundColor: "teal",
+        padding: "30px",
+        color: 'white'
+    },
+    flexibleContainer:{
+        margin: "10px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        padding: "30px"
+    },
+    icons: {
+        margin: "10px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        padding: "30px",
+        fontSize: "3rem",
+    }
+}
 export const Footer = () => {
-  return <footer>
-    <ul>
+  return <footer style={customStyle.footer}>
+    <h1 style={{fontSize: "30px", color: "blue", textAlign: "center"}}>A Reusable Footer Component</h1>
+    <ul style={customStyle.flexibleContainer}>
         <li>Fitness Dashboard</li>
         <li>Watch Videos</li>
         <li>Privacy Policy</li>
     </ul>
-    <ul>
+    <ul style={customStyle.flexibleContainer}>
         <li>Services</li>
         <li>Discord</li>
         <li>Terms & Conditions</li>
     </ul>
     <ul>
         <li>
-            <p>© 2025 Fitness Dashboard. All Rights Reserved.</p>
+            <p style={{textAlign:"center"}}>© 2025 Fitness Dashboard. All Rights Reserved.</p>
         </li>
-        <li>Happy Coding</li>
     </ul>
-    <ul>
+    <ul style={customStyle.icons}>
         <li><a href="#">🎮</a></li>
         <li><a href="#">🐓</a></li>
         <li><a href="#">💻</a></li>
