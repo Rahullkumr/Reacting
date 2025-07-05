552ad1d0917df5b178d7383f205b771ad84bf075
Rahul Kumar <kumarrahul771996@gmail.com>
2025-07-06 04:29:22 +0530

    update readme

--- FirstProject/src/App.jsx
+++ FirstProject/src/App.jsx
@@ -4,20 +4,25 @@ import { Navbar } from './day02/ReusableNavbar'
 import { Footer } from './day02/ReusableFooter'
 import { Card } from './day02/ReusableProfileCard'
 import { MyApp } from './day02/ReusableProfileCard'
+import Practice from './Practice'
 
 function App() {
 
   return (
+    // day01
     // <div class= "flex items-center justify-center h-screen bg-teal-900">
     //   <h1 class="text-3xl font-bold text-white">Hello React 👋</h1>
     // </div>
 
     // day02 
-    <div>
-      {/* <Navbar/> */}
-      {/* <Footer/> */}
-      <MyApp/>
-    </div>
+    // <div>
+    //   <Navbar/>
+    //   <Footer/>
+    //   <MyApp/>
+    // </div>
+
+    // day03
+    <Practice/>
   )
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ React JS practice
 - pnpm + vite + React
 - React 19
 - TailwindCSS 4 (always use official website, don't rely on chatGPT)
+- React Router v7 (declarative mode)
