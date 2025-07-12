29c4dd2ea67a8872ce7af7c53cd8dbbe6fb3bd45
Rahul Kumar <kumarrahul771996@gmail.com>
2025-07-12 22:49:50 +0530

    add: auto fixes by prettier

--- FirstProject/src/App.jsx
+++ FirstProject/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import { Navbar } from './day02/ReusableNavbar'
-import { Footer } from './day02/ReusableFooter'
-import { Card } from './day02/ReusableProfileCard'
-import { MyApp } from './day02/ReusableProfileCard'
-import Practice from './Practice'
+import { useState } from "react";
+import "./App.css";
+import { Navbar } from "./day02/ReusableNavbar";
+import { Footer } from "./day02/ReusableFooter";
+import { Card } from "./day02/ReusableProfileCard";
+import { MyApp } from "./day02/ReusableProfileCard";
+import Practice from "./Practice";
 
 function App() {
-
   return (
     // day01
     // <div class= "flex items-center justify-center h-screen bg-teal-900">
     //   <h1 class="text-3xl font-bold text-white">Hello React 👋</h1>
     // </div>
 
-    // day02 
+    // day02
     // <div>
     //   <Navbar/>
     //   <Footer/>
@@ -22,8 +21,8 @@ function App() {
     // </div>
 
     // day03
-    <Practice/>
-  )
+    <Practice />
+  );
 }
 
-export default App
+export default App;
