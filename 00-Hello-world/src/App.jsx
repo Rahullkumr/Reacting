a80c86f7febde28046be757f8e9f5f49071175f5
Rahul Kumar <kumarrahul771996@gmail.com>
2025-07-26 18:43:27 +0530

    add: hello world project with animation

---
+++ 00-Hello-world/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-black text-white flex justify-center items-center">
+      <h1 className="text-4xl animate-bounce">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
